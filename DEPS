10979532926a64c8107150596c65cd4729a22fb5
bradnelson@google.com <bradnelson@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2010-05-24 05:35:43 +0000

    Fixing up cygwin build of nethack. Adding chromium cygwin as a temporary measure (for gcc + flex). Switched to explicit flex + bison. Fixed cmd files to use hermetic cygwin second (so installed cygwin or cygwin 1.7 from sdk is preferred).
    BUG=None
    TEST=None
    TBR=nfullagar
    
    Review URL: http://codereview.chromium.org/2077018
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@26 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- DEPS
+++ DEPS
@@ -5,6 +5,13 @@ vars = {
 deps = {
 }
 
+deps_os = {
+  "win": {
+    "src/third_party/cygwin":
+      "http://src.chromium.org/svn/trunk/deps/third_party/cygwin@11984",
+  },
+}
+
 hooks = [
   {
     "pattern": ".",
--- packages/nacl-clean-repository.cmd
+++ packages/nacl-clean-repository.cmd
@@ -2,7 +2,7 @@
 
 setlocal
 
-PATH=%~dp0..\third_party\cygwin\bin;%PATH%
+PATH=%PATH%;%~dp0..\third_party\cygwin\bin
 
 ln -sfn %~dp0..\toolchain\win_x86\nacl /nacl 2>nul
 ln -sfn %~dp0..\toolchain\win_x86\nacl64 /nacl64 2>nul
