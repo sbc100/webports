59950d7d778a7f054cb408b8b67c96920c3ed123
sanga@google.com <sanga@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2010-07-07 20:48:56 +0000

    Using default extraction step.
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@46 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- packages/scripts/gtest-1.5.0/nacl-gtest-1.5.0.sh
+++ packages/scripts/gtest-1.5.0/nacl-gtest-1.5.0.sh
@@ -34,7 +34,7 @@ CustomConfigureStep() {
 CustomInstallStep() {
   Remove ${NACL_SDK_USR_INCLUDE}/gtest
   readonly THIS_PACKAGE_PATH=${NACL_PACKAGES_REPOSITORY}/${PACKAGE_NAME}
-  (ChangeDir include; tar cf - --exclude='gtest-death-test.h' gtest | (ChangeDir ${NACL_SDK_USR_INCLUDE}; tar xfp -))
+  (ChangeDir include; tar cf - --exclude='gtest-death-test.h' --exclude='gtest-death-test-internal.h' gtest | (ChangeDir ${NACL_SDK_USR_INCLUDE}; tar xfp -))
   Remove ${NACL_SDK_USR_LIB}/${LIB_GTEST}
   install --mode=644 ${LIB_GTEST} ${NACL_SDK_USR_LIB}/${LIB_GTEST}
 }
