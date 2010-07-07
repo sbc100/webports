9c5888b6e6a601e3ed31fa5873a7660bd882d5eb
sanga@google.com <sanga@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2010-07-07 21:56:42 +0000

    Adding GMock to NaClPorts Review URL: http://codereview.chromium.org/2805076
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@48 7dad1e8b-422e-d2af-fbf5-8013b78bd812

---
+++ packages/scripts/gmock-1.5.0/nacl-gmock-1.5.0.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Copyright (c) 2010 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that be
+# found in the LICENSE file.
+#
+
+# nacl-gmock-1.5.0.sh
+#
+# usage:  nacl-gmock-1.5.0.sh
+#
+# this script downloads, patches, and builds boost for Native Client
+#
+
+readonly URL=http://build.chromium.org/mirror/nacl/gmock-1.5.0.tgz
+#readonly URL=http://googlemock.googlecode.com/files/gmock-1.5.0.tar.gz
+readonly PATCH_FILE=gmock-1.5.0/nacl-gmock-1.5.0.patch
+readonly PACKAGE_NAME=gmock-1.5.0
+
+source ../common.sh
+
+CustomConfigureStep() {
+  Banner "Configuring ${PACKAGE_NAME}"
+  # export the nacl tools
+  export CC=${NACLCC}
+  export CXX=${NACLCXX}
+  export AR=${NACLAR}
+  export RANLIB=${NACLRANLIB}
+  export PATH=${NACL_BIN_PATH}:${PATH};
+  export LIB_GMOCK=libgmock.a
+
+  ChangeDir ${NACL_PACKAGES_REPOSITORY}/${PACKAGE_NAME}
+}
+
+CustomInstallStep() {
+  Remove ${NACL_SDK_USR_INCLUDE}/gmock
+  readonly THIS_PACKAGE_PATH=${NACL_PACKAGES_REPOSITORY}/${PACKAGE_NAME}
+  (ChangeDir include; tar cf - gmock | (ChangeDir ${NACL_SDK_USR_INCLUDE}; tar xfp -))
+  Remove ${NACL_SDK_USR_LIB}/${LIB_GMOCK}
+  install --mode=644 ${LIB_GMOCK} ${NACL_SDK_USR_LIB}/${LIB_GMOCK}
+}
+
+CustomPackageInstall() {
+   DefaultPreInstallStep
+   DefaultDownloadStep
+   DefaultExtractStep
+   DefaultPatchStep
+   CustomConfigureStep
+   DefaultBuildStep
+   CustomInstallStep
+   DefaultCleanUpStep
+}
+
+CustomPackageInstall
+exit 0
