f1c70066f1ca3ef8b3a4b4f5e2c7424f73922553
dspringer@google.com <dspringer@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2011-01-11 20:21:50 +0000

    Fix FreeImage build on Mac & Linux.
    Review URL: http://codereview.chromium.org/6163004
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@149 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- packages/scripts/FreeImage-3.14.1/nacl-FreeImage-3.14.1.sh
+++ packages/scripts/FreeImage-3.14.1/nacl-FreeImage-3.14.1.sh
@@ -17,6 +17,18 @@ readonly URL=http://commondatastorage.googleapis.com/nativeclient-mirror/nacl/Fr
 readonly PATCH_FILE=FreeImage-3.14.1/nacl-FreeImage-3.14.1.patch
 readonly PACKAGE_BASE_NAME=FreeImage
 readonly PACKAGE_NAME=${PACKAGE_BASE_NAME}-3.14.1
+# This list of files needs to have CRLF (Windows)-style line endings translated
+# to LF (*nix)-style line endings prior to applying the patch.  This list of
+# files is taken from nacl-FreeImage-3.14.1.patch.
+readonly -a CRLF_TRANSLATE_FILES=(
+    "Makefile"
+    "Source/LibOpenJPEG/opj_includes.h"
+    "Source/LibRawLite/dcraw/dcraw.c"
+    "Source/LibRawLite/internal/defines.h"
+    "Source/LibRawLite/libraw/libraw.h"
+    "Source/LibRawLite/src/libraw_cxx.cpp"
+    "Source/OpenEXR/Imath/ImathMatrix.h"
+    "Source/Utilities.h")
 
 source ../common.sh
 
@@ -42,6 +54,18 @@ CustomExtractStep() {
   ChangeDir ${NACL_PACKAGES_REPOSITORY}
   Remove ${PACKAGE_NAME}
   unzip ${NACL_PACKAGES_TARBALLS}/${PACKAGE_NAME}.zip
+  # FreeImage uses CRLF for line-endings.  The patch file has LF (Unix-style)
+  # line endings, which means on some versions of patch, the patch fails. Run a
+  # recursive tr over all the sources to remedy this.
+  ChangeDir ${NACL_PACKAGES_REPOSITORY}/${PACKAGE_BASE_NAME}
+  # Setting LC_CTYPE is a Mac thing.  The locale needs to be set to "C" so that
+  # tr interprets the '\r' string as ASCII and not UTF-8.
+  export
+  for crlf in ${CRLF_TRANSLATE_FILES[@]}; do
+    echo "tr -d '\r' < ${crlf}"
+    LC_CTYPE=C tr -d '\r' < ${crlf} > .tmp
+    mv .tmp ${crlf}
+    done
 }
 
 CustomConfigureStep() {
