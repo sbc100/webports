d48aed34ddf54d14603716dfcb742a898164ef82
mcgrathr@chromium.org <mcgrathr@chromium.org@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2011-06-08 21:50:04 +0000

    Move -lnosys to end of link lines
    This moves -lnosys from the beginning to the end of the linking order
    in two places that had it wrong (quake and expat).
    
    BUG=none
    TEST=none
    
    R=dmichael@google.com,bradnelson@google.com,nfullagar@google.com
    
    Review URL: http://codereview.chromium.org/7046060
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@296 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- packages/scripts/expat-2.0.1/nacl-expat-2.0.1.sh
+++ packages/scripts/expat-2.0.1/nacl-expat-2.0.1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Copyright (c) 2009 The Native Client Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that be
+# Copyright (c) 2011 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
 
@@ -8,7 +8,7 @@
 #
 # usage:  nacl-expat-2.0.1.sh
 #
-# this script downloads, patches, and builds expat for Native Client 
+# this script downloads, patches, and builds expat for Native Client
 #
 
 readonly URL=http://commondatastorage.googleapis.com/nativeclient-mirror/nacl/expat-2.0.1.tar.gz
@@ -18,7 +18,7 @@ readonly PACKAGE_NAME=expat-2.0.1
 
 source ../common.sh
 
-export LDFLAGS=-lnosys
+export LIBS=-lnosys
 
 DefaultPackageInstall
 exit 0
