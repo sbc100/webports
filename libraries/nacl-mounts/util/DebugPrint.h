33ba23c2d6ce3339d5b2b672c9b30b871c638bc9
eugenis@google.com <eugenis@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2011-09-13 13:52:21 +0000

    Add a dbgprintf() that goes through __real_write.
    Normal printf() deadlocks on the big lock of the KernelProxy when used inside
    nacl-mounts.
    Review URL: http://codereview.chromium.org/7867037
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@435 7dad1e8b-422e-d2af-fbf5-8013b78bd812

---
+++ libraries/nacl-mounts/util/DebugPrint.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <stdio.h>
+#include <stdarg.h>
+
+extern "C" {
+  ssize_t __real_write(int fd, const void *buf, size_t count);
+}
+
+static int dbgprintf(const char* format, ...) {
+  const int buf_size = 1000;
+  char buf[buf_size];
+  va_list args;
+  va_start(args, format);
+  ssize_t r = vsnprintf(buf, buf_size, format, args);
+  if (r > 0)
+    __real_write(2, buf, r);
+  va_end(args);
+  return r;
+}
