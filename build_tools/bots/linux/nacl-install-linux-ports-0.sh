8b08a5e32543be47dcf6c837b217d44b6830e44e
dpolukhin@google.com <dpolukhin@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2012-01-13 06:38:37 +0000

    Add openssl to buildbot #0 Review URL: http://codereview.chromium.org/9190050
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@504 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- build_tools/bots/linux/nacl-install-linux-ports-0.sh
+++ build_tools/bots/linux/nacl-install-linux-ports-0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2011 The Native Client Authors. All rights reserved.
+# Copyright (c) 2012 The Native Client Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
@@ -30,6 +30,7 @@ BuildPackage sdl_net
 BuildPackage sdl_mixer
 BuildPackage scummvm
 BuildPackage bochs
+BuildPackage openssl
 
 echo "@@@BUILD_STEP ${NACL_PACKAGES_BITSIZE}-bit Summary@@@"
 if [[ $RESULT != 0 ]] ; then
