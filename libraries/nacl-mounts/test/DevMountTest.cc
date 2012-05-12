6f0c8c390f9258a618b449d2ce10da1810aad56d
vissi@google.com <vissi@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2012-05-12 07:10:04 +0000

    fix DevMountTest on mac Review URL: https://chromiumcodereview.appspot.com/10391086
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@583 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- libraries/nacl-mounts/test/DevMountTest.cc
+++ libraries/nacl-mounts/test/DevMountTest.cc
@@ -8,19 +8,19 @@
 #include "gtest/gtest.h"
 
 TEST(DevMountTest, Sanity) {
-  DevMount* bm = new DevMount();
+  DevMount* dm = new DevMount();
 
   // just make sure all of the calls run
   struct stat st;
   struct dirent dir;
-  EXPECT_EQ(bm->GetNode("/", &st), 0);
-  EXPECT_EQ(0, bm->Stat(1, &st));
+  EXPECT_EQ(dm->GetNode("/", &st), 0);
+  EXPECT_EQ(0, dm->Stat(1, &st));
   EXPECT_EQ(1, S_ISDIR(st.st_mode));
-  EXPECT_GT(bm->Getdents(1, 0, &dir, 1024), 1);
-  bm->GetNode("/null", &st);
+  EXPECT_GT(dm->Getdents(1, 0, &dir, sizeof(struct dirent)*2), 1);
+  dm->GetNode("/null", &st);
   int node = st.st_ino;
   char buf[10];
-  EXPECT_EQ(5, bm->Write(node, 0, reinterpret_cast<void*>(buf), 5));
-  EXPECT_EQ(0, bm->Isatty(0));
+  EXPECT_EQ(5, dm->Write(node, 0, reinterpret_cast<void*>(buf), 5));
+  EXPECT_EQ(0, dm->Isatty(0));
 }
 
