920707aaefb0e1c77484ba749144cc213ab61a18
olonho@google.com <olonho@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2012-09-07 12:07:15 +0000

    Simplify building for newlib, using glibc emulation layer. Review URL: https://chromiumcodereview.appspot.com/10908055
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@640 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- libraries/openssl-1.0.0e/nacl-openssl-1.0.0e.sh
+++ libraries/openssl-1.0.0e/nacl-openssl-1.0.0e.sh
@@ -18,13 +18,17 @@ readonly PACKAGE_NAME=openssl-1.0.0e
 
 source ../../build_tools/common.sh
 
-
 CustomConfigureStep() {
   Banner "Configuring ${PACKAGE_NAME}"
   local extra_args=""
   if [[ "${NACL_PACKAGES_BITSIZE}" = "pnacl" ||
 	"${NACL_TOOLCHAIN_ROOT}" == *newlib* ]] ; then
-    extra_args+=" no-dso -DOPENSSL_NO_SOCK=1 "
+    readonly GLIBC_COMPAT=${NACL_SDK_USR_INCLUDE}/glibc-compat
+    if [[ ! -f ${GLIBC_COMPAT}/netdb.h ]]; then
+      echo "Please install glibc-compat first"
+      exit 1
+    fi
+    extra_args+=" no-dso -DOPENSSL_NO_SOCK=1 -I${GLIBC_COMPAT}"
   fi
 
   ChangeDir ${NACL_PACKAGES_REPOSITORY}/${PACKAGE_NAME}
@@ -32,54 +36,9 @@ CustomConfigureStep() {
      --prefix=${NACL_SDK_USR} no-asm no-hw no-krb5 ${extra_args} -D_GNU_SOURCE
 }
 
-# This is a little brutal but gives us a nice way to track progress.
-# We need to indentify the files which other project
-# may need and then make sure they can be builte with newlib,
-#
-# Note: things that are likely needed: crypto/rand/rand_unix.c
 CustomHackStepForNewlib() {
-  local bad="crypto/bio/bss_fd.c
-             crypto/bio/bss_sock.c
-             crypto/bio/bss_conn.c
-             crypto/bio/b_sock.c
-             crypto/bio/bss_acpt.c
-             crypto/bio/bss_log.c
-             crypto/bio/bss_dgram.c
-             crypto/rand/rand_egd.c
-             crypto/rand/rand_unix.c
-             crypto/ui/ui_openssl.c
-             crypto/pkcs7/bio_pk7.c
-             ssl/d1_lib.c"
-
-  echo "patching ssl/ssl_locl.h"
-  # removing this include makes the crucial files
-  # ssl/s3_pkt.c and ssl/s23_pkt.c and the not so crucial file
-  # ssl/d1_pkt.c compile
-  # but for some reason just clearing out e_os.h does not work
-  # Note: pretty much all the other files listed as bad do include
-  # e_os.h via some level of indirection as well
-  sed -i -e 's/#include "e_os.h"/#define clear_sys_error() errno=0/' \
-      ssl/ssl_locl.h
-
-  for f in ${bad} ; do
-    echo "newlib hack removing: $f"
-    rm $f
-    echo "/* removed - newlib incompatible */" > $f
-  done
-
-  echo "patching crypto/rand/rand_unix.c"
-  echo
-  echo "IMPORTAMT: someone with a clue needs to check/fix this!"
-  echo "           What is the entropy source for openssl in nacl?"
-  echo
-  echo "int RAND_poll(void) { return 0; } " > crypto/rand/rand_unix.c
-
-  # this is probably ok
-  echo "patching crypto/ui/ui_openssl.c"
-  echo "void* UI_OpenSSL(void) { return 0; } " > crypto/ui/ui_openssl.c
-
-
-  # These two makefiles build binaries which we do not care about
+  # These two makefiles build binaries which we do not care about,
+  # and they depend on -ldl, so cannot be built with newlib.
   echo "all clean install: " > apps/Makefile
   echo "all clean install: " > test/Makefile
 }
@@ -97,7 +56,7 @@ CustomPackageInstall() {
   CustomConfigureStep
   if [[ "${NACL_PACKAGES_BITSIZE}" = "pnacl" ||
 	"${NACL_TOOLCHAIN_ROOT}" == *newlib* ]] ; then
-      CustomHackStepForNewlib
+    CustomHackStepForNewlib
   fi
   CustomBuildStep
   DefaultInstallStep
