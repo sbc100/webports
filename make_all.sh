695c5ed9faed35a86161e845ff95be6ad4fc89eb
sbc@google.com <sbc@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2013-01-24 18:19:26 +0000

    Add make_all.sh script to build all configuration
    This can be run to build all ports for all architecture
    and C library combination.
    
    I can also be used to build all combinations of just
    one port.  e.g:
    
    $ ./build_all.sh png
    
    R=binji
    BUG=None
    
    Review URL: https://codereview.chromium.org/12041070
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@683 7dad1e8b-422e-d2af-fbf5-8013b78bd812

---
+++ make_all.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# Copyright (c) 2012 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# This script builds all naclports in all possible configurations.
+# If you want to be sure your change won't break anythign this is
+# a good place to start.
+
+set -x
+set -e
+
+TARGETS="$*"
+TARGETS=${TARGETS:-all}
+
+# x86_64 NaCL
+export NACL_ARCH=x86_64
+export NACL_GLIBC=1
+make ${TARGETS}
+unset NACL_GLIBC
+make ${TARGETS}
+
+# i686 NaCL
+export NACL_ARCH=i686
+export NACL_GLIBC=1
+make ${TARGETS}
+unset NACL_GLIBC
+make ${TARGETS}
+
+# PNaCl
+export NACL_ARCH=pnacl
+make ${TARGETS}
+
+# ARM
+# Not all ports build for ARM yet.
+# TODO(sbc): fix the ARM ports.
+#export NACL_ARCH=arn
+#make ${TARGETS}
