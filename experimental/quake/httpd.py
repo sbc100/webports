1d06dbec25520cae5509fbc78112b24d502bef10
sbc@google.com <sbc@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2013-03-19 00:12:13 +0000

    Don't hardcode Python path
    Instead of hardcoding /usr/bin/python, which might be set to point to
    Python3 for many users, execute `/usr/bin/env python` in the she-bang;
    users simply need to be instructed to make sure that this points to
    Python2.
    
    Review URL: https://codereview.chromium.org/12412021
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@713 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- experimental/quake/httpd.py
+++ experimental/quake/httpd.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #
 # Copyright 2010, The Native Client SDK Authors.  All Rights Reserved.
 # Use of this source code is governed by a BSD-style license that can
