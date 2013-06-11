73bee06e6634d64c17ce7feea8cf85074e4194c5
sbc@google.com <sbc@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2013-06-11 22:45:55 +0000

    Implement SDLmain for nacl.
    Using -lSDLmain allows unmodified SDL application to run
    on NaCl.  SDLmain take case of creating the instance and
    enqueuing SDL event as well as creating a thread on which
    to run the application's main function.
    
    Also, fix sdl-config to report correct libraries for
    linking with SDL
    
    Also, enable building and publishing of SDL test
    applications.
    
    Fix crash in SDL_quit.
    
    BUG=https://code.google.com/p/naclports/issues/detail?id=73
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/16764003
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@782 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- libraries/SDL/nacl-SDL.sh
+++ libraries/SDL/nacl-SDL.sh
@@ -14,8 +14,6 @@
 source pkg_info
 source ../../build_tools/common.sh
 
-export LIBS=-lnosys
-
 AutogenStep() {
   ChangeDir ${NACL_PACKAGES_REPOSITORY}/${PACKAGE_NAME}
   # For some reason if we don't remove configure before running
@@ -27,6 +25,44 @@ AutogenStep() {
   PatchConfigure
   PatchConfigSub
 }
+
+
+ConfigureTests() {
+  Banner "Configuring ${PACKAGE_NAME}"
+  ChangeDir ${NACL_PACKAGES_REPOSITORY}/${PACKAGE_NAME}
+
+  # export the nacl tools
+  export CC=${NACLCC}
+  export CXX=${NACLCXX}
+  export AR=${NACLAR}
+  export RANLIB=${NACLRANLIB}
+  export PKG_CONFIG_PATH=${NACLPORTS_LIBDIR}/pkgconfig
+  export PKG_CONFIG_LIBDIR=${NACLPORTS_LIBDIR}
+  export PATH=${NACL_BIN_PATH}:${PATH};
+
+  local conf_host=${NACL_CROSS_PREFIX}
+  if [ ${NACL_ARCH} = "pnacl" ]; then
+    # The PNaCl tools use "pnacl-" as the prefix, but config.sub
+    # does not know about "pnacl".  It only knows about "le32-nacl".
+    # Unfortunately, most of the config.subs here are so old that
+    # it doesn't know about that "le32" either.  So we just say "nacl".
+    conf_host="nacl"
+  fi
+
+  Remove ${NACL_BUILD_SUBDIR}-test
+  MakeDir ${NACL_BUILD_SUBDIR}-test
+  cd ${NACL_BUILD_SUBDIR}-test
+
+  LIBS="$LDFLAGS" LogExecute ../test/configure \
+    --host=${conf_host} \
+    --disable-assembly \
+    --disable-pthread-sem \
+    --prefix=${NACLPORTS_PREFIX} \
+    --exec-prefix=${NACLPORTS_PREFIX} \
+    --libdir=${NACLPORTS_LIBDIR} \
+    --oldincludedir=${NACLPORTS_INCLUDE}
+}
+
 
 CustomConfigureStep() {
   Banner "Configuring ${PACKAGE_NAME}"
@@ -41,16 +77,21 @@ CustomConfigureStep() {
   export PKG_CONFIG_LIBDIR=${NACLPORTS_LIBDIR}
   export PATH=${NACL_BIN_PATH}:${PATH};
 
-  # LIBS and LDFLAGS are needed when linking the shared library.
-  export LIBS="-lppapi_cpp"
-  export LDFLAGS="$LDFLAGS -Wl,--as-needed"
+  local conf_host=${NACL_CROSS_PREFIX}
+  if [ ${NACL_ARCH} = "pnacl" ]; then
+    # The PNaCl tools use "pnacl-" as the prefix, but config.sub
+    # does not know about "pnacl".  It only knows about "le32-nacl".
+    # Unfortunately, most of the config.subs here are so old that
+    # it doesn't know about that "le32" either.  So we just say "nacl".
+    conf_host="nacl-pnacl"
+  fi
 
   Remove ${NACL_BUILD_SUBDIR}
   MakeDir ${NACL_BUILD_SUBDIR}
   cd ${NACL_BUILD_SUBDIR}
 
   LogExecute ../configure \
-    --host=nacl \
+    --host=${conf_host} \
     --disable-assembly \
     --disable-pthread-sem \
     --prefix=${NACLPORTS_PREFIX} \
@@ -58,6 +99,26 @@ CustomConfigureStep() {
     --libdir=${NACLPORTS_LIBDIR} \
     --oldincludedir=${NACLPORTS_INCLUDE}
 }
+
+
+PublishStep() {
+  local PUBLISH_DIR=${NACL_PACKAGES_PUBLISH}/sdl-tests/${NACL_ARCH}-${NACL_LIBC}
+  Banner "Publishing Tests ${PUBLISH_DIR}"
+  Remove ${PUBLISH_DIR}
+  MakeDir ${PUBLISH_DIR}
+  LogExecute cp *.?exe ${PUBLISH_DIR}
+  cd ../test
+  LogExecute cp *.bmp *.wav *.xbm *.dat *.txt ${PUBLISH_DIR}
+  ChangeDir ${PUBLISH_DIR}
+  # Older versions of the SDK don't include create_html.py so we
+  # need to check for its existence.
+  if [ -e "${NACL_SDK_ROOT}/tools/create_html.py" ]; then
+    for NEXE in *.?exe; do
+      LogExecute "${NACL_SDK_ROOT}/tools/create_html.py" ${NEXE}
+    done
+  fi
+}
+
 
 CustomPackageInstall() {
   DefaultPreInstallStep
@@ -68,8 +129,12 @@ CustomPackageInstall() {
   CustomConfigureStep
   DefaultBuildStep
   DefaultInstallStep
+  ConfigureTests
+  DefaultBuildStep
+  PublishStep
   DefaultCleanUpStep
 }
+
 
 CustomPackageInstall
 exit 0
