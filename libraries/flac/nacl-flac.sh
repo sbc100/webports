d509eba6a8e5c207eee0274e17fa5fef61f4ec42
sbc@google.com <sbc@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2013-08-23 17:20:16 +0000

    Remove patches for missing inet headers.
    Recent versions of the NaCl SDK now include these.
    
    Also, use DefaultConfigureStep in freeimage build.
    
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/23333004
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@877 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- libraries/flac/nacl-flac.sh
+++ libraries/flac/nacl-flac.sh
@@ -8,49 +8,18 @@ source ../../build_tools/common.sh
 
 
 ConfigureStep() {
-  Banner "Configuring ${PACKAGE_NAME}"
-  # export the nacl tools
-  export CC=${NACLCC}
-  export CXX=${NACLCXX}
-  export AR=${NACLAR}
-  export RANLIB=${NACLRANLIB}
-  export PKG_CONFIG_PATH=${NACLPORTS_PREFIX}/lib/pkgconfig
-  export PKG_CONFIG_LIBDIR=${NACLPORTS_PREFIX}/lib
-  export PATH=${NACL_BIN_PATH}:${PATH};
-  export LIBS="-lnosys"
-  ChangeDir ${NACL_PACKAGES_REPOSITORY}/${PACKAGE_NAME}
-  MakeDir ${NACL_BUILD_SUBDIR}
-  ChangeDir ${NACL_BUILD_SUBDIR}
-  ../configure \
-    --host=nacl \
-    --prefix=${NACLPORTS_PREFIX} \
-    --exec-prefix=${NACLPORTS_PREFIX} \
-    --libdir=${NACLPORTS_PREFIX}/lib \
-    --oldincludedir=${NACLPORTS_PREFIX}/include \
-    --enable-sse \
-    --disable-3dnow \
-    --disable-altivec \
-    --disable-thorough-tests \
-    --disable-oggtest \
-    --disable-xmms-plugin \
-    --without-metaflac-test-files \
-    --disable-asm-optimizations \
-    --with-http=off \
-    --with-html=off \
-    --with-ftp=off \
-    --with-x=no
-
+  if [ "${NACL_GLIBC}" != 1 ]; then
+    export LIBS="-lglibc-compat -lnosys"
+  fi
+  EXTRA_CONFIGURE_ARGS+=" --disable-oggtest"
+  EXTRA_CONFIGURE_ARGS+=" --disable-xmms-plugin"
+  EXTRA_CONFIGURE_ARGS+=" --without-metaflac-test-files"
+  DefaultConfigureStep
   PostConfigureStep
 }
 
 
 PostConfigureStep() {
-  # add stub includes
-  mkdir netinet
-  touch netinet/in.h
-  # add ntohl for Native Client
-  echo "#define ntohl(x) ((((x)>>24)&0xFF)|(((x)>>8)&0xFF00)|\
-(((x)<<8)&0xFF0000)|(((x)<<24)&0xFF000000))" >> config.h
   # satisfy random, srandom
   echo "/* pull features.h that has __GLIBC__ */" >> config.h
   echo "#include <stdlib.h>" >> config.h
