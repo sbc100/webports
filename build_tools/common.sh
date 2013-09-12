5f03a351ec29f61a3a2a9f5dcc28cd45f8d8fce4
sbc@google.com <sbc@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2013-09-12 15:59:30 +0000

    Fix sed command so it work with mac (BSD) and GNU sed.
    TBR=binji
    TEST= run commond on both mac and linux
    
    Review URL: https://codereview.chromium.org/24076009
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@898 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- build_tools/common.sh
+++ build_tools/common.sh
@@ -300,7 +300,7 @@ PatchSpecFile() {
   # For newlib toolchain, modify the specs file to give an error when attempting
   # to create a shared object.
   if [ "${NACL_GLIBC}" != "1" ]; then
-    sed -i '' "s/%{shared:-shared/%{shared:%e${ERROR_MSG}/" "${SPECS_FILE}"
+    sed -i='' "s/%{shared:-shared/%{shared:%e${ERROR_MSG}/" "${SPECS_FILE}"
   fi
 }
 
