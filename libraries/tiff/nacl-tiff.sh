e320ad7eff010203c98a5b2ef09bd0c1667b0521
jvoung@google.com <jvoung@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2013-10-10 22:27:15 +0000

    Remove -O2 workaround for libtiff + PNaCl.
    Unstick the NaClPorts bots slightly, since there's
    currently a failure using CFLAGS after r913:
    
    ./nacl-tiff.sh: line 14: CFLAGS: unbound variable
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3599
    R=bradnelson@google.com
    
    Review URL: https://codereview.chromium.org/26164006
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@916 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- libraries/tiff/nacl-tiff.sh
+++ libraries/tiff/nacl-tiff.sh
@@ -8,12 +8,6 @@ source ../../build_tools/common.sh
 
 export LIBS="-lnosys -lm"
 
-# TODO: Remove when this is fixed.
-# https://code.google.com/p/nativeclient/issues/detail?id=3599
-if [ "$NACL_ARCH" = "pnacl" ]; then
-  export NACLPORTS_CFLAGS="${NACLPORTS_CFLAGS//-O2/}"
-fi
-
 CONFIG_SUB=config/config.sub
 DefaultPackageInstall
 exit 0
