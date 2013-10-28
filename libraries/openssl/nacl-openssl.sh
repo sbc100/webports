df6c71f00706baab87f2e16c87f64156c8c4cb90
jvoung@google.com <jvoung@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2013-10-28 18:15:31 +0000

    Build and run the openssl unittests.
    Background: An external developer is seeing slowdown in
    the NaCl builds compared to the host builds. One hypothesis
    is that NaCl's ILP32 model means that even on x86-64
    machines the NaCl build does not do 64-bit loads-stores
    so we see a hit with that. Before doing anything drastic
    with the build, start running some unit tests!
    (preliminary tests show that 64-bit arith can cut the
    running time of a benchmark by 30%).
    
    For Glibc, we just need to set up the sel_ldr scripts.
    Ideally this would build the tests, write the sel_ldr
    scripts, then run "make test" from the top level.  However,
    writing the sel_ldr scripts require we know the list of
    tests still... unless the compiler were to generate scripts
    as it builds.
    
    For Newlib, we need to configure the build differently so
    that "-ldl" is not used to link (won't work that way!).
    Some of the tests won't link without socket() and
    shutdown(), so skip those for now. A couple of them also
    segfault with both nacl-gcc and pnacl.
    
    BUG= https://code.google.com/p/naclports/issues/detail?id=88
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/37463005
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@941 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- libraries/openssl/nacl-openssl.sh
+++ libraries/openssl/nacl-openssl.sh
@@ -9,6 +9,7 @@ source ../../build_tools/common.sh
 ConfigureStep() {
   Banner "Configuring ${PACKAGE_NAME}"
   local EXTRA_ARGS=""
+  local machine="i686"
   if [ "${NACL_GLIBC}" != "1" ] ; then
     local GLIBC_COMPAT=${NACLPORTS_INCLUDE}/glibc-compat
     if [ ! -f ${GLIBC_COMPAT}/netdb.h ]; then
@@ -16,19 +17,23 @@ ConfigureStep() {
       exit 1
     fi
     EXTRA_ARGS+=" no-dso -I${GLIBC_COMPAT}"
+    # The default from MACHINE=i686 is linux-elf, which links things
+    # with -ldl. However, newlib does not have -ldl. In that case,
+    # make a fake machine where the build rule does not use -ldl.
+    machine="le32newlib"
   fi
 
   ChangeDir ${NACL_PACKAGES_REPOSITORY}/${PACKAGE_NAME}
-  MACHINE=i686 CC=${NACLCC} AR=${NACLAR} RANLIB=${NACLRANLIB} ./config \
-     --prefix=${NACLPORTS_PREFIX} no-asm no-hw no-krb5 ${EXTRA_ARGS} -D_GNU_SOURCE
+  MACHINE=${machine} CC=${NACLCC} AR=${NACLAR} RANLIB=${NACLRANLIB} ./config \
+    --prefix=${NACLPORTS_PREFIX} no-asm no-hw no-krb5 ${EXTRA_ARGS} \
+    -D_GNU_SOURCE
 }
 
 
 HackStepForNewlib() {
-  # These two makefiles build binaries which we do not care about,
-  # and they depend on -ldl, so cannot be built with newlib.
+  # This makefile link programs that require socket(), etc.
+  # Stub them out until we link against nacl_io or something.
   echo "all clean install: " > apps/Makefile
-  echo "all clean install: " > test/Makefile
 }
 
 
@@ -40,6 +45,39 @@ BuildStep() {
   make build_libs
 }
 
+TestStep() {
+  echo "Running OpenSSL TestStep"
+  # TODO(jvoung): Integrate better with "make test".
+  # We'd need to make util/shlib_wrap.sh run the sel_ldr scripts instead
+  # of trying to run the nexes directly.
+  local all_tests="bntest ectest ecdsatest ecdhtest exptest \
+ideatest shatest sha1test sha256t sha512t mdc2test rmdtest md2test md4test \
+md5test hmactest wp_test rc2test rc4test rc5test bftest casttest \
+destest randtest dhtest dsatest ssltest rsa_test enginetest igetest \
+jpaketest srptest asn1test"
+  local passing_tests="${all_tests}"
+  pushd test
+  # Newlib can't build ssltest -- requires socket()
+  # md2test, rc5test, jpaketest just segfaults w/ nacl-gcc-newlib and pnacl.
+  if [ "${NACL_GLIBC}" != "1" ]; then
+    local broken_tests="md2test rc5test ssltest jpaketest"
+    for to_filter in ${broken_tests}; do
+      passing_tests=$(echo ${passing_tests} | sed "s/\b${to_filter}//g")
+    done
+    make CC=${NACLCC} ${passing_tests}
+    # Special case -- needs an input file =(
+    make CC=${NACLCC} evp_test
+  else
+    # Plain make works better and doesn't muck with CC, etc.
+    # Otherwise, we end up missing -ldl for GLIBC...
+    make
+  fi
+  for test_name in ${passing_tests}; do
+    RunSelLdrCommand ${test_name}
+  done
+  RunSelLdrCommand evp_test evptests.txt
+  popd test
+}
 
 PackageInstall() {
   PreInstallStep
@@ -51,6 +89,7 @@ PackageInstall() {
     HackStepForNewlib
   fi
   BuildStep
+  TestStep
   InstallStep
 }
 
