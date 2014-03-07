10b63585eece9b3fd15d75f494a9328441150820
binji@chromium.org <binji@chromium.org@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2014-03-07 22:36:24 +0000

    Fix building freetype w/ Glibc.
    builds/unix/configure needs to be patched explicitly.
    
    BUG=none
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/191053005
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1152 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- ports/freetype/build.sh
+++ ports/freetype/build.sh
@@ -10,3 +10,10 @@
 #   either set the LIBPNG_CFLAGS and LIBPNG_LDFLAGS environment variables,
 #   or pass `--without-png' to the `configure' script.
 EXTRA_CONFIGURE_ARGS="--without-png"
+
+PatchStep() {
+  DefaultPatchStep
+  ChangeDir ${SRC_DIR}
+  Banner "Patching configure"
+  ${TOOLS_DIR}/patch_configure.py builds/unix/configure
+}
