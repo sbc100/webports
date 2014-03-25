e02f140e1b4db493a268f2d159ba9d5b5917362b
bradnelson@google.com <bradnelson@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2014-03-25 00:04:51 +0000

    Fixing gdb slow start and dropping nethack specific code.
    Gdb uses libiberty which makes the assumption that vfork calls actually work.
    It compounds this mistake by using an invocation pattern for vfork in which it
    sleeps and retries in the event of failure. As vfork does not currently work,
    disabling the code path for a faster start.
    
    BUG=None
    TEST=forthcoming (other CL)
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/208423009
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1175 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- ports/gdb/gdb_pepper.c
+++ ports/gdb/gdb_pepper.c
@@ -18,21 +18,6 @@ int gdb_pepper_main(int argc, char *argv[]);
 int gdb_pepper_main(int argc, char* argv[]) {
   umount("/");
   mount("foo", "/", "memfs", 0, NULL);
-  // TODO(bradnelson): Don't bake in the assumption of debugging only nethack.
-  mkdir("/mnt", 0777);
-  mount("http://localhost:5103/nethack-3.4.3/glibc",
-        "/mnt/nethack", "httpfs", 0,
-        "manifest=/nethack.manifest,"
-        "allow_cross_origin_requests=true,allow_credentials=false");
-
-  // TODO(bradnelson): Figure out why passing these in by -ex doesn't work.
-  printf("These are the commands to debug nethack:\n\n");
-  printf("nacl-manifest /mnt/nethack/nethack/nethack_debug.nmf\n");
-  printf("set directories /mnt/nethack/nethack_src/src\n");
-  printf("break nethack_main\n");
-  printf("target remote :4014\n");
-  printf("cont\n");
-  printf("\n");
 
   return gdb_nacl_main(argc, argv);
 }
