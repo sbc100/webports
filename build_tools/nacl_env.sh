d02fa841329eaf9d5342fcd84d4290e0c2a9ade4
bradnelson@google.com <bradnelson@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2014-04-02 18:44:48 +0000

    Avoid linuxism in build for emacs, enable for x86-64 mac.
    The emacs build used the -n option to echo which is not supported on mac.
    Avoiding that pattern.
    We've started shipping a 64-bit sel_ldr for mac, enabling it in naclports.
    
    BUG=None
    TEST=None
    R=petewil@chromium.org, sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/222503002
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1200 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- build_tools/nacl_env.sh
+++ build_tools/nacl_env.sh
@@ -100,15 +100,14 @@ fi
 if [ ${NACL_ARCH} = "i686" ]; then
   readonly NACL_SEL_LDR=${NACL_SDK_ROOT}/tools/sel_ldr_x86_32
   readonly NACL_IRT=${NACL_SDK_ROOT}/tools/irt_core_x86_32.nexe
+elif [ ${NACL_ARCH} = "x86_64" ]; then
+  readonly NACL_SEL_LDR=${NACL_SDK_ROOT}/tools/sel_ldr_x86_64
+  readonly NACL_IRT=${NACL_SDK_ROOT}/tools/irt_core_x86_64.nexe
 elif [ ${NACL_ARCH} = "pnacl" ]; then
   readonly NACL_SEL_LDR_X8632=${NACL_SDK_ROOT}/tools/sel_ldr_x86_32
   readonly NACL_IRT_X8632=${NACL_SDK_ROOT}/tools/irt_core_x86_32.nexe
   readonly NACL_SEL_LDR_X8664=${NACL_SDK_ROOT}/tools/sel_ldr_x86_64
   readonly NACL_IRT_X8664=${NACL_SDK_ROOT}/tools/irt_core_x86_64.nexe
-elif [ ${NACL_ARCH} = "x86_64" -a ${OS_NAME} != "Darwin" ]; then
-  # No 64-bit sel_ldr on darwin today.
-  readonly NACL_SEL_LDR=${NACL_SDK_ROOT}/tools/sel_ldr_x86_64
-  readonly NACL_IRT=${NACL_SDK_ROOT}/tools/irt_core_x86_64.nexe
 fi
 
 # NACL_CROSS_PREFIX is the prefix of the executables in the
