61bc1c7692e96916136cbe63bcc00a62e7879db1
binji@chromium.org <binji@chromium.org@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2014-04-08 18:28:53 +0000

    Fix linking issues with DevIL and PNaCl.
    "extern inline" and "inline" have reversed behaviors depending on whether you
    are compiling in Gnu89 mode or C99 mode. DevIL checks for __GNUC__ to determine
    whether it should use "extern inline" or "inline", but unfortunately LLVM
    defines this but has different default behavior. This change forces LLVM to use
    Gnu89 mode, where extern inline will not generate a global symbol (but may add
    a reference to one).
    
    Also, in libpng, png_set_gray_1_2_4_to_8 was renamed to to
    png_set_expand_gray_1_2_4_to_8. I've updated nacl.patch to use this instead. It
    seems the upstream DevIL has not been fixed.
    
    BUG=none
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/228973002
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1214 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- ports/devil/build.sh
+++ ports/devil/build.sh
@@ -3,6 +3,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+if [ "${NACL_ARCH}" = "pnacl" ]; then
+  NACLPORTS_CFLAGS+=" -std=gnu89"
+fi
+
 TestStep() {
   return 0
 
