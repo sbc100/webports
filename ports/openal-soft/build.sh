7c162e0cd5e506ef503aa1e87802d1572eb27330
sbc@google.com <sbc@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2014-04-12 00:39:54 +0000

    openal: Disable use of cpuid.h on i686
    The cpuid.h that we ship in the toolchain contains asm that
    doesn't validate.  So disable it for now.
    
    Also add EXECUTABLES=openal-info so that the binary get
    validate and we catch validations earlier.
    
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=3836
    R=nfullagar@chromium.org
    
    Review URL: https://codereview.chromium.org/235873004
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1224 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- ports/openal-soft/build.sh
+++ ports/openal-soft/build.sh
@@ -3,7 +3,17 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+EXECUTABLES=openal-info
+
+# Our cpuid.h file for i686 contains asm that doesn't validate so for
+# now we disable use of cpuid.h.
+# TODO(sbc): re-enable once this get fixed:
+# https://code.google.com/p/nativeclient/issues/detail?id=3836
+if [ "${NACL_ARCH}" = "i686" ]; then
+  EXTRA_CMAKE_ARGS+=" -DHAVE_CPUID_H=0"
+fi
+
 # Defaults to dynamic lib, but newlib can only link statically.
 if [ "${NACL_LIBC}" = "newlib" ]; then
-  EXTRA_CMAKE_ARGS="-DLIBTYPE=STATIC"
+  EXTRA_CMAKE_ARGS+=" -DLIBTYPE=STATIC"
 fi
