3550a8404b79eeb2a931464730ac7429ae24c14b
sbc@google.com <sbc@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2014-05-23 18:49:42 +0000

    Re-add gtest port.
    This is disabled by default since gtest ships with the SDK
    itself, but its still useful to be able to build it locally
    within naclports.
    
    R=bradnelson@google.com
    
    Review URL: https://codereview.chromium.org/291333008
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1286 7dad1e8b-422e-d2af-fbf5-8013b78bd812

---
+++ ports/gtest/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright (c) 2011 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+CONFIG_SUB=build-aux/config.sub
+
+ExtractStep() {
+  DefaultExtractStep
+  # Add chmod here since gtest archive contains readonly files we don't
+  # want installed files to be readonly
+  LogExecute chmod -R +w ${SRC_DIR}
+}
+
+InstallStep() {
+  MakeDir ${DESTDIR_LIB}
+  MakeDir ${DESTDIR_INCLUDE}
+
+  LogExecute install -m 644 lib/.libs/libgtest.a ${DESTDIR_LIB}/
+  if [ "${NACL_GLIBC}" = "1" ]; then
+    LogExecute install -m 644 lib/.libs/libgtest.so* ${DESTDIR_LIB}/
+  fi
+
+  LogExecute cp -r ${SRC_DIR}/include/gtest ${DESTDIR_INCLUDE}/gtest
+}
