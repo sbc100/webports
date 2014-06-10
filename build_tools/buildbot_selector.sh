c6bc379c83285283bf2fe055b094f37d6d31a315
sbc@google.com <sbc@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2014-06-10 23:10:27 +0000

    Don't upload binaries from the nightly builders.
    We were checking from periodic- rather than nightly-.
    
    R=bradnelson@google.com, bradnelson
    
    Review URL: https://codereview.chromium.org/327083002
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1309 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- build_tools/buildbot_selector.sh
+++ build_tools/buildbot_selector.sh
@@ -70,10 +70,7 @@ Publish() {
 }
 
 # Don't upload periodic or trybot builds.
-if [[ ${BUILDBOT_BUILDERNAME} =~ periodic-* ]]; then
-  NACLPORTS_NO_UPLOAD=1
-fi
-if [[ ${BUILDBOT_BUILDERNAME} =~ naclports-* ]]; then
+if [[ ${BUILDBOT_BUILDERNAME} =~ (nightly-*|naclports-*) ]]; then
   NACLPORTS_NO_UPLOAD=1
 fi
 
