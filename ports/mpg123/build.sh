4719096e4a5daa2b467a7a7540928c9cd12c480c
sbc@google.com <sbc@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2014-06-14 00:07:35 +0000

    Update mpg123 to 1.20.0 and cleanup the build script.
    Specficially this change now allows building as shared
    library under glibc.
    
    R=bradnelson@google.com
    
    Review URL: https://codereview.chromium.org/333653004
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1328 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- ports/mpg123/build.sh
+++ ports/mpg123/build.sh
@@ -6,30 +6,23 @@
 # TODO(binji): Use assembly
 EXTRA_CONFIGURE_ARGS="--enable-static -with-cpu=generic_fpu"
 
-if [ "${NACL_LIBC}" != "glibc" ]; then
+if [ "${NACL_SHARED}" = "1" ]; then
+  EXE_DIR=src/.libs
+else
+  EXE_DIR=src
+fi
+
+EXECUTABLES="${EXE_DIR}/mpg123${NACL_EXEEXT} \
+  ${EXE_DIR}/out123${NACL_EXEEXT} \
+  ${EXE_DIR}/mpg123-id3dump${NACL_EXEEXT} \
+  ${EXE_DIR}/mpg123-strip${NACL_EXEEXT}"
+
+if [ "${NACL_LIBC}" = "newlib" ]; then
   # Disable network support for newlib builds.
   # TODO(sbc): remove this once network syscalls land in libnacl
   EXTRA_CONFIGURE_ARGS+=" --enable-network=no"
+  # sigaction is not defined by newlib.
+  NACLPORTS_CPPFLAGS+=" -DDONT_CATCH_SIGNALS"
 fi
 
-NACLPORTS_LDFLAGS="${NACLPORTS_LDFLAGS} -static"
-
-BuildStep() {
-  export PATH=${NACL_BIN_PATH}:${PATH}
-
-  ChangeDir src/libmpg123
-  LogExecute make clean
-  LogExecute make -j${OS_JOBS}
-
-  Banner "Building Tests"
-  local tests="tests/seek_accuracy${NACL_EXEEXT} tests/seek_whence${NACL_EXEEXT} tests/text${NACL_EXEEXT}"
-
-  ChangeDir ..
-  LogExecute make clean
-  LogExecute make -j${OS_JOBS} ${tests}
-}
-
-InstallStep() {
-  ChangeDir src/libmpg123
-  DefaultInstallStep
-}
+NACLPORTS_LDFLAGS="${NACLPORTS_LDFLAGS} -pthread"
