ff9530137a5585e56d6335907e6a10514015b9d9
channingh@google.com <channingh@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2014-06-16 22:10:55 +0000

    Change make to have a PPAPI entry point and use nacl-spawn.
    BUG=None
    TESTS=None
    R=bradnelson@google.com
    
    Review URL: https://codereview.chromium.org/332293002
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1331 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- ports/make/build.sh
+++ ports/make/build.sh
@@ -8,3 +8,15 @@ CONFIG_SUB=config/config.sub
 if [ "${NACL_LIBC}" = "newlib" ]; then
   NACLPORTS_CPPFLAGS+=" -D_POSIX_VERSION"
 fi
+
+InstallStep() {
+  MakeDir ${PUBLISH_DIR}
+  cp make${NACL_EXEEXT} ${PUBLISH_DIR}/make_${NACL_ARCH}${NACL_EXEEXT}
+
+  pushd ${PUBLISH_DIR}
+  LogExecute python ${NACL_SDK_ROOT}/tools/create_nmf.py \
+      ${PUBLISH_DIR}/make_*${NACL_EXEEXT} \
+      -s . \
+      -o make.nmf
+  popd
+}
