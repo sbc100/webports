6d3fd84dde88a428d1d2f8e3b2c9456bdb4c03e4
bradnelson@google.com <bradnelson@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2014-07-21 16:25:12 +0000

    Adding port of m4 for newlib, glibc, pnacl.
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3902
    TEST=./make_all.sh m4
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/382323002
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1459 7dad1e8b-422e-d2af-fbf5-8013b78bd812

---
+++ ports/m4/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright (c) 2014 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+export EXTRA_LIBS="${NACL_CLI_MAIN_LIB} -lppapi_simple -lnacl_spawn \
+  -lnacl_io -lppapi -lppapi_cpp -l${NACL_CPP_LIB}"
+
+if [ "${NACL_LIBC}" = "newlib" ]; then
+  NACLPORTS_CPPFLAGS+=" -I${NACLPORTS_INCLUDE}/glibc-compat"
+fi
+
+NACLPORTS_CPPFLAGS+=" -DGNULIB_defined_struct_sigaction"
+
+PatchStep() {
+  DefaultPatchStep
+  # Touch documentation to prevent it from updating.
+  touch ${SRC_DIR}/doc/*
+}
+
+InstallStep() {
+  PublishByArchForDevEnv
+}
