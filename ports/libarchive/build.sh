9c56006d4af74bd7f16cd9c2a5e5f5e7181f8790
cmihail@chromium.org <cmihail@chromium.org@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2014-07-24 00:32:51 +0000

    Add libarchive.
    Libarchive (http://www.libarchive.org/) is required for decompressing rar
    archives. Most of the library was ported successfuly, with the exception of
    bsdtar and bsdcpio. Unfortunately, make throws a few warnings, especially for
    libarchive/archive_read_disk_posix.c and libarchive/archive_write_disk_posix.c
    (e.g. implicit declaration of function 'fchdir' with all error being similar to
    this one). I was unable to resolve these issues, but maybe nacl_io library might
    help.
    
    The library was tested with rar and zip archives for decompressing and it works
    correctly. I have used only custom read functions (see
    https://github.com/libarchive/libarchive/wiki/Examples#List_contents_of_Archive_with_custom_read_functions).
    The posix based API for reading files from disk might fail (not tested).
    But the custom functions should be enough for most needs.
    
    The make check command for testing fails because of similar errors as above.
    
    TEST=./make_all.sh libarchive
    BUG=388077
    R=mtomasz@chromium.org, sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/398893005
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1468 7dad1e8b-422e-d2af-fbf5-8013b78bd812

---
+++ ports/libarchive/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Copyright (c) 2014 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+EXTRA_CONFIGURE_ARGS="--disable-bsdtar --disable-bsdcpio"
+
+NACLPORTS_CPPFLAGS+=" -Dtimezone=_timezone"
+
+# Necessary for libxml2.
+export LIBS="-lpthread -lm"
