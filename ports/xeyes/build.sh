7b21846802e37401b7639c9572875bf5597fc5ec
bradnelson@google.com <bradnelson@google.com@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2014-07-29 00:58:12 +0000

    Adding xeyes program.
    BUG=None
    TEST=local
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/428763002
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1494 7dad1e8b-422e-d2af-fbf5-8013b78bd812

---
+++ ports/xeyes/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright (c) 2014 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+NACLPORTS_CPPFLAGS+=" -Dmain=nacl_main"
+
+export LIBS+=" -lSM -lICE -lxcb -lXau \
+  -Wl,--undefined=nacl_main ${NACL_CLI_MAIN_LIB} \
+  -lppapi_simple -lnacl_io -lppapi -lppapi_cpp -l${NACL_CPP_LIB}"
+
+if [ "${NACL_LIBC}" = "newlib" ]; then
+  NACLPORTS_CPPFLAGS+=" -I${NACLPORTS_INCLUDE}/glibc-compat"
+  LIBS+=" -lglibc-compat"
+fi
+
+InstallStep() {
+  PublishByArchForDevEnv
+}
