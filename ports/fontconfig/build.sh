8879557c449aecaa67d3eb8f8593090718524422
phosek@chromium.org <phosek@chromium.org@7dad1e8b-422e-d2af-fbf5-8013b78bd812>
2014-08-22 16:53:50 +0000

    Update fontconfig to version 2.11.1
    This is needed for the new texlive which depends on the latest version of fontconfig.
    
    BUG=none
    TEST=FORCE=1 FROM_SOURCE=1 ./make_all.sh fontconfig
    R=bradnelson@google.com, sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/476843002
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1567 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- ports/fontconfig/build.sh
+++ ports/fontconfig/build.sh
@@ -4,8 +4,3 @@
 # found in the LICENSE file.
 
 EXTRA_CONFIGURE_ARGS="--disable-docs --with-arch=x86"
-
-# The configure script for fontconfig uses AC_PATH_PROG rather than
-# AC_PATH_TOOL when looking for freetype-config, so it doesn't finds the
-# system one rather than one in the toolchain.
-EXTRA_CONFIGURE_ARGS+=" --with-freetype-config=${NACL_PREFIX}/bin/freetype-config"
