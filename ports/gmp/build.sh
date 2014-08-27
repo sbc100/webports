e69223d9d5611e16ed1b1cab2b8f7973feef51c6
sbc@google.com <sbc@google.com>
2014-08-27 11:08:36 +0000

    Fix gmp ARM build by definining LONGLONG_STANDALONE.
    Without this longlong.h will on ARM will reference
    machine dependent functions which are not compiled.
    
    TBR=bradnelson@google.com
    
    Review URL: https://codereview.chromium.org/515473002
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1581 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- ports/gmp/build.sh
+++ ports/gmp/build.sh
@@ -7,5 +7,12 @@ if [ "${NACL_SHARED}" = "1" ]; then
   NACLPORTS_CFLAGS+=" -fPIC"
 fi
 
+# define LONGLONG_STANDALONE so that longlong.h doesn't
+# contain references to machine-dependant functions.
+# This is needed in particular on ARM but is correct on
+# all platforms since we don't compile machine-depenedant
+# files.
+NACLPORTS_CFLAGS+=" -DLONGLONG_STANDALONE"
+
 # Disable all assembly code by specifying none-none-none.
 EXTRA_CONFIGURE_ARGS=--host=none-none-none
