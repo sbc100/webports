6a0f3de07c46861d46c97c79617cbbdc1e682f5a
bradnelson@google.com <bradnelson@google.com>
2014-08-28 16:26:19 +0000

    Adding blackbox window manager port.
    BUG=None
    TEST='FORCE=1 FROM_SOURCE=1 ./make_all.sh blackbox'
    R=sbc@chromium.org, binji@chromium.org
    
    Review URL: https://codereview.chromium.org/517473002
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1586 7dad1e8b-422e-d2af-fbf5-8013b78bd812

---
+++ ports/blackbox/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright (c) 2014 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+EXTRA_CONFIGURE_ARGS+=" --with-x"
+EXTRA_CONFIGURE_ARGS+=" --x-includes=${NACLPORTS_INCLUDE}"
+EXTRA_CONFIGURE_ARGS+=" --x-libraries=${NACLPORTS_LIBDIR}"
+
+EXECUTABLES="src/blackbox util/bsetroot util/bstyleconvert"
+
+export EXTRA_LIBS+="\
+  -liconv \
+  -lXext -lXmu -lSM -lICE -lXt -lX11 -lxcb -lXau \
+  -Wl,--undefined=nacl_main ${NACL_CLI_MAIN_LIB} \
+  -lppapi_simple -lnacl_io -lppapi -lppapi_cpp -l${NACL_CPP_LIB}"
+
+if [ "${NACL_LIBC}" = "newlib" ]; then
+  NACLPORTS_CPPFLAGS+=" -I${NACLPORTS_INCLUDE}/glibc-compat"
+  export LIBS+=" -lglibc-compat"
+  # List again at the end of the others to resolve use of getservbyname in an X
+  # library.
+  export EXTRA_LIBS+=" -lglibc-compat"
+fi
+
+InstallStep() {
+  PublishByArchForDevEnv
+}
