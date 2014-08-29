92fd713673a3ba25629ea67a81b717e91a1d93c9
sbc@google.com <sbc@google.com>
2014-08-29 16:12:23 +0000

    Remove TODO from libtiff build script.
    The pnacl compiler was fixed in the pepper_32 release.
    
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/524533003
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1595 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- ports/tiff/build.sh
+++ ports/tiff/build.sh
@@ -3,13 +3,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-
-# TODO(sbc): Remove this once 32 becomes stable
-# https://code.google.com/p/nativeclient/issues/detail?id=3599
-if [ "$NACL_ARCH" = "pnacl" -a ${NACL_SDK_VERSION} -lt 32 ]; then
-  NACLPORTS_CFLAGS="${NACLPORTS_CFLAGS//-O2/}"
-fi
-
 export LIBS="-lm"
 
 CONFIG_SUB=config/config.sub
