27d401fd683f8765f6279ef968fa816d4001e4af
sbc@google.com <sbc@google.com>
2014-09-09 19:36:01 +0000

    Revert to non-parallel build for subversion.
    It was occationally flaking on the bots due to a lack
    of make dependency between of the object files and the
    build output directories being created.
    
    R=bradnelson@google.com
    
    Review URL: https://codereview.chromium.org/559613003
    
    git-svn-id: https://naclports.googlecode.com/svn/trunk/src@1644 7dad1e8b-422e-d2af-fbf5-8013b78bd812

--- ports/subversion/build.sh
+++ ports/subversion/build.sh
@@ -2,6 +2,12 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# subversion's build system seem to have a bug with out-of-tree builds
+# and parallel building that was causing occational flakes on the buildbots.
+# The mkdir-init target appears to be required before any object file can be
+# built but none of the object files seem to depend on this.
+OS_JOBS=1
+
 NACLPORTS_CPPFLAGS+=" -Dmain=nacl_main"
 
 export LIBS+=" -Wl,--undefined=nacl_main ${NACL_CLI_MAIN_LIB} -lppapi_simple \
