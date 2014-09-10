8e5067c57f1841e66fc40c162e19450062d12c55
Sam Clegg <sbc@chromium.org>
2014-09-09 20:36:53 -0700

    Update x264 version.
    Also, set $EXECTUTABLES variable.
    
    R=bradnelson@google.com
    
    Review URL: https://codereview.chromium.org/525043002

--- ports/x264/build.sh
+++ ports/x264/build.sh
@@ -3,13 +3,14 @@
 # found in the LICENSE file.
 
 BUILD_DIR=${SRC_DIR}
+EXECUTABLES=x264${NACL_EXEEXT}
 
 ConfigureStep() {
   SetupCrossEnvironment
 
   local conf_host
   if [ "${NACL_ARCH}" = pnacl ]; then
-    conf_host=nacl
+    conf_host=nacl-pnacl
   else
     conf_host=${NACL_CROSS_PREFIX}
   fi
@@ -17,10 +18,9 @@ ConfigureStep() {
   LogExecute ./configure \
     --host=${conf_host} \
     --prefix=${PREFIX} \
-    --cross-prefix=${NACL_CROSS_PREFIX} \
+    --cross-prefix=${NACL_CROSS_PREFIX}- \
     --disable-asm \
-    --disable-pthread \
-    --extra-ldflags="-lm"
+    --disable-thread
 
-  make clean
+  LogExecute make clean
 }
