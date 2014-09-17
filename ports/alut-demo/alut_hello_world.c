c1dd863bd2059ad994d9be4dbed37926265428e8
Sam Clegg <sbc@chromium.org>
2014-09-17 16:04:31 -0700

    Revive alut demo application.
    TEST=run example and verify "hello world" plays.
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/582473002

--- ports/alut-demo/alut_hello_world.c
+++ ports/alut-demo/alut_hello_world.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <AL/alut.h>
 
-#include <ppapi_main/ppapi_main.h>
+#include <ppapi_simple/ps_main.h>
 #include <ppapi/c/ppb.h>
 
 /*
@@ -12,12 +12,10 @@
   Link using '-lalut -lopenal -lpthread'.
 */
 
-PPAPI_MAIN_WITH_DEFAULT_ARGS
-
 void alSetPpapiInfo(PP_Instance instance, PPB_GetInterface get_interface);
 
 int
-ppapi_main(int argc, const char *argv[])
+nacl_main(int argc, char **argv)
 {
   ALuint helloBuffer, helloSource;
 
@@ -25,9 +23,9 @@ ppapi_main(int argc, const char *argv[])
    * This extra line is required by the underlying openAl
    * NaCl port.
    */
-  alSetPpapiInfo (PPAPI_GetInstanceId(), PPAPI_GetInterface);
+  alSetPpapiInfo (PSGetInstanceId(), PSGetInterface);
 
-  alutInit (&argc, (char**)argv);
+  alutInit (&argc, argv);
   helloBuffer = alutCreateBufferHelloWorld ();
   alGenSources (1, &helloSource);
   alSourcei (helloSource, AL_BUFFER, helloBuffer);
@@ -36,3 +34,5 @@ ppapi_main(int argc, const char *argv[])
   alutExit ();
   return EXIT_SUCCESS;
 }
+
+PPAPI_SIMPLE_REGISTER_MAIN(nacl_main)
--- ports/freealut/build.sh
+++ ports/freealut/build.sh
@@ -2,5 +2,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+if [ "${NACL_SHARED}" = "1" ]; then
+  EXECUTABLES="examples/.libs/playfile examples/.libs/hello_world"
+else
+  EXECUTABLES="examples/playfile examples/hello_world"
+fi
 
+# Without this openal is not detected
 export LIBS="-lm"
