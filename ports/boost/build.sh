5d175903748781a7a80819cc0cef3abff40e6e47
Sam Clegg <sbc@chromium.org>
2014-09-23 18:44:25 -0700

    Update boost build script.
    - Enable far more of boost for newlib/pnacl builds.
    - Make boost build aware of NACLPORTS_CPPFLAGS
    - Build boost in parallel
    - Install boost correctly via package rather than directly
      into the toolchain
    - For cross compilation to unix (rather than building for host OS)
    
    R=bradnelson@google.com
    
    Review URL: https://codereview.chromium.org/601613002

--- ports/boost/build.sh
+++ ports/boost/build.sh
@@ -4,28 +4,37 @@
 
 BUILD_DIR=${SRC_DIR}
 
-BUILD_ARGS="\
+if [ "${NACL_LIBC}" = "newlib" ]; then
+  NACLPORTS_CPPFLAGS+=" -I${NACLPORTS_INCLUDE}/glibc-compat"
+fi
+
+# TODO(eugenis): build dynamic libraries, too
+BUILD_ARGS="
+  target-os=unix
   --build-dir=../${NACL_BUILD_SUBDIR} \
   --stagedir=../${NACL_BUILD_SUBDIR} \
   link=static"
 
-# TODO(eugenis): build dynamic libraries, too
-if [ "${NACL_LIBC}" = "glibc" ] ; then
-  BUILD_ARGS+=" --without-python --without-signals --without-mpi"
-  BUILD_ARGS+=" --without-context --without-coroutine"
-else
-  BUILD_ARGS+=" --with-date_time --with-program_options"
+BUILD_ARGS+=" --without-python"
+BUILD_ARGS+=" --without-signals"
+BUILD_ARGS+=" --without-mpi"
+BUILD_ARGS+=" --without-context"
+BUILD_ARGS+=" --without-coroutine"
+
+if [ "${NACL_LIBC}" = "newlib" ] ; then
+  BUILD_ARGS+=" --without-locale"
 fi
 
 ConfigureStep() {
-  echo "using gcc : 4.4.3 : ${NACLCXX} ;" > tools/build/v2/user-config.jam
+  echo "using gcc : 4.4.3 : ${NACLCXX} <compileflags>${NACLPORTS_CPPFLAGS};" \
+      >> tools/build/v2/user-config.jam
   LogExecute ./bootstrap.sh --prefix=${NACL_PREFIX}
 }
 
 BuildStep() {
-  LogExecute ./b2 stage ${BUILD_ARGS}
+  LogExecute ./b2 stage -j ${OS_JOBS} ${BUILD_ARGS}
 }
 
 InstallStep() {
-  LogExecute ./b2 install ${BUILD_ARGS}
+  LogExecute ./b2 install -d0 --prefix=${DESTDIR}/${PREFIX} ${BUILD_ARGS}
 }
