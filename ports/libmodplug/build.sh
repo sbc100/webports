68aa657d646dbcd19441f10b087241234450aa72
Sam Clegg <sbc@chromium.org>
2014-09-29 17:50:40 -0700

    Fix libmodplug build
    libmodplug needs to use glibc-compat since it uses
    random/srandom.
    
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/612073004

---
+++ ports/libmodplug/build.sh
@@ -0,0 +1,8 @@
+# Copyright 2014 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+if [ "${NACL_LIBC}" = "newlib" ]; then
+  NACLPORTS_CPPFLAGS+=" -I${NACLPORTS_INCLUDE}/glibc-compat"
+  export LIBS="-lglibc-compat"
+fi
