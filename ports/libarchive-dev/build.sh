31ab0cca24513053618fd030fe8a1d42c4fe60a7
Tomasz Mikolajewski <mtomasz@chromium.org>
2014-10-30 09:44:22 +0900

    Add a developer libarchive build.
    Patch by Mihail Costea. The original description is below:
    
    > The developer version uses directly the github repository. Required for
    > testing new features of libarchive like ZIP64.
    
    > Github cut was made after next commit:
    > https://github.com/libarchive/libarchive/commit/da906f19fc74cb8826c67a3e6671fb4ac0e1939b
    
    BUG=chromium:366642
    R=sbc@chromium.org
    TBR=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/635243002

---
+++ ports/libarchive-dev/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Copyright (c) 2014 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# TODO(mtomasz): Remove this package, once a new upstream release of libarchive
+# is available.
+
+AutogenStep() {
+  ChangeDir ${SRC_DIR}
+  export MAKE_LIBARCHIVE_RELEASE="1"
+  ./build/autogen.sh
+  cd -
+}
+
+ConfigureStep() {
+  AutogenStep
+
+  EXTRA_CONFIGURE_ARGS="--disable-bsdtar --disable-bsdcpio"
+  EXTRA_CONFIGURE_ARGS+=" --without-iconv"
+
+  # Temporary xml2 support cannot be added because the patch used in
+  # ports/libarchve doesn't apply correctly here due. The reason is that
+  # configure file is not present on gihub repository and is created
+  # after AutogenStep.
+  # # TODO(cmihail): Remove this once nacl.patch is applied correctly.
+  EXTRA_CONFIGURE_ARGS+=" --without-xml2"
+
+  NACLPORTS_CPPFLAGS+=" -Dtimezone=_timezone"
+
+  DefaultConfigureStep
+}
