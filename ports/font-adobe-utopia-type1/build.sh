c77aece469f7cfb7842c29029f0170636b2a590a
Sam Clegg <sbc@chromium.org>
2014-11-12 17:05:50 -0800

    Fix location of host build of font-util
    I recently changes this location but failed to spot
    that is was being refernced in all these places.
    
    R=bradnelson@google.com
    
    Review URL: https://codereview.chromium.org/716313002

--- ports/font-adobe-utopia-type1/build.sh
+++ ports/font-adobe-utopia-type1/build.sh
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-PATH="${BUILD_DIR}/../../font-util/install-nacl-host/bin:${PATH}"
+PATH="${BUILD_DIR}/../../font-util/install_host/bin:${PATH}"
 
 # Disable running on mkfontdir/mkfontscale on install.
 export ac_cv_path_MKFONTDIR=echo
--- ports/font-jis-misc/build.sh
+++ ports/font-jis-misc/build.sh
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-PATH="${BUILD_DIR}/../../font-util/install-nacl-host/bin:${PATH}"
+PATH="${BUILD_DIR}/../../font-util/install_host/bin:${PATH}"
 
 # Setting this as the fonts don't seem to honor --prefix for everything.
 EXTRA_CONFIGURE_ARGS="--with-fontrootdir=${PREFIX}/share/fonts/X11"
