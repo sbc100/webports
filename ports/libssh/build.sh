71462892931d0c9b27b0a97b3893711b359275c5
Sam Clegg <sbc@chromium.org>
2014-11-14 16:02:35 -0800

    Fix libssh build by disabling -fstack-protector
    The NaCl x86 toolchains currently fail to at
    link time if -fstack-protector due to lack of
    libssp.
    
    R=bradnelson@google.com
    
    Review URL: https://codereview.chromium.org/729793002

--- ports/libssh/build.sh
+++ ports/libssh/build.sh
@@ -2,6 +2,17 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+if [ "${NACL_LIBC}" = "newlib" ]; then
+  NACLPORTS_CPPFLAGS+=" -I${NACLPORTS_INCLUDE}/glibc-compat"
+fi
+
+# Workaround for arm-gcc bug:
+# https://code.google.com/p/nativeclient/issues/detail?id=3205
+# TODO(sbc): remove this once the issue is fixed
+if [ "${NACL_ARCH}" = "arm" ]; then
+  NACLPORTS_CPPFLAGS+=" -mfpu=vfp"
+fi
+
 EXTRA_CMAKE_ARGS="\
   -DPNACL=ON\
   -DWITH_STATIC_LIB=ON\
