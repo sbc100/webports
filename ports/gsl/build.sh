b5e508e63336e8050b8b668ac148ca58e4698820
Sam Clegg <sbc@chromium.org>
2014-12-01 11:50:29 -0800

    gsl: new upstream version 1.9 -> 1.16
    Also add TestStep for this gsl.
    
    R=bradnelson@google.com, bradnelson
    
    Review URL: https://codereview.chromium.org/770653002

--- ports/gsl/build.sh
+++ ports/gsl/build.sh
@@ -2,9 +2,13 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-ConfigureStep() {
-  export LIBS="-lm"
+EXECUTABLES="
+  gsl-randist${NACL_EXEEXT}
+  gsl-histogram${NACL_EXEEXT}
+  sys/test${NACL_EXEEXT}
+"
 
+ConfigureStep() {
   # TODO(sbc): Remove when this is fixed.
   # https://code.google.com/p/nativeclient/issues/detail?id=3205
   if [ "$NACL_ARCH" = "arm" ]; then
@@ -12,4 +16,13 @@ ConfigureStep() {
   fi
 
   DefaultConfigureStep
+}
+
+BuildStep() {
+  DefaultBuildStep
+  LogExecute make -C sys test${NACL_EXEEXT}
+}
+
+TestStep() {
+  LogExecute ./sys/test
 }
