978ff9e74c7b25fdf904b532711e730496d46537
Sam Clegg <sbc@chromium.org>
2015-01-13 11:25:22 -0800

    Require a specific and recent version pip
    The pip_install command was failing in strange ways
    due to variation in the behavior of pip itself
    between versions.
    
    For example, older versions don't support the
    --no-compile flag.
    
    Also, with the recent versions --download-cache
    is no longer supported or needed (its automatic).
    
    R=bradnelson@google.com
    
    Review URL: https://codereview.chromium.org/819843003

--- build_tools/pip_install.sh
+++ build_tools/pip_install.sh
@@ -4,37 +4,35 @@
 # found in the LICENSE file.
 
 # Installs required python modules using 'pip'.
-# If 'pip' executable is not found in PATH then install that first.
-# Installs are performed with the --user argument which means that
-# root privileges are not required and PYTHONUSERBASE is set to point
-# to the local 'out' directory so installs are local to naclports.
+# Always use a locally installed version of pip rather than relying
+# on the system version since the behaviour and command line flags
+# for pip inself vary between versions.
 
 SCRIPT_DIR="$(cd $(dirname $0) && pwd)"
+ARGS="--user --no-compile"
+
 cd "${SCRIPT_DIR}/.."
 
 export PYTHONUSERBASE=$PWD/out/pip
-export PATH=$PATH:$PYTHONUSERBASE/bin
+pip_bin_dir=$PYTHONUSERBASE/bin
+pip_bin=$pip_bin_dir/pip
+export PATH=$PATH:$pip_bin_dir
 
-pip=$(which pip)
-set -e
-if [ -z "$pip" ]; then
+if [ ! -f "$pip_bin" ]; then
+  # On first run install pip directly from the network
   echo "Installing pip.."
   # Use local file rather than pipeline so we can detect failure of the curl
   # command.
   curl --silent --show-error https://bootstrap.pypa.io/get-pip.py > get-pip.py
-  python get-pip.py --user
+  python get-pip.py --force-reinstall --user
   rm -f get-pip.py
-fi
-
-ARGS="--download-cache=out/cache/pip --user -r requirements.txt"
-
-# pip version 1.5 and above support the --no-compile option which we prefer
-# here as it makes the output less verbose, and our modules will be compiled
-# on first use anyway
-pip_version=$(pip --version | cut -d ' ' -f 2)
-if [[ $pip_version =~ ^1.5 ]]; then
-  ARGS+=" --no-compile"
+  hash -r
 fi
 
 set -x
-pip install $ARGS
+# Pin locally install pip to a specific version
+pip install ${ARGS} "pip==6.0.6"
+
+# At this point we know we have good pip install in $PATH and we can use
+# it to install the requirements.
+pip install ${ARGS} -r requirements.txt
