7c0e9f731977a5274807ebd95d39833dd9875a6b
Sam Clegg <sbc@chromium.org>
2015-01-18 07:41:05 -0800

    Temporarily disable gdb tests
    BUG= https://code.google.com/p/naclports/issues/detail?id=187
    TBR=bradnelson
    
    Review URL: https://codereview.chromium.org/854363002

--- ports/gdb/build.sh
+++ ports/gdb/build.sh
@@ -126,6 +126,9 @@ InstallStep() {
 }
 
 PostInstallTestStep() {
+  # Temporartily disabled until this gets fixed:
+  # https://code.google.com/p/naclports/issues/detail?id=187
+  return
   if [[ ${OS_NAME} == Darwin && ${NACL_ARCH} == x86_64 ]]; then
     echo "Skipping gdb/debug tests on unsupported mac + x86_64 configuration."
   elif [[ ${NACL_ARCH} == arm ]]; then
