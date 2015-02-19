86433f80b866db4ff7463dafb8d92d79f2a3a25c
Sam Clegg <sbc@chromium.org>
2015-02-19 14:30:08 -0800

    Fix libxpm build (LIB variable was being clobbered)
    TEST=./make_all.sh libxpm
    R=bradnelson@google.com
    
    Review URL: https://codereview.chromium.org/940163004

--- ports/libxpm/build.sh
+++ ports/libxpm/build.sh
@@ -4,11 +4,13 @@
 
 if [ "${NACL_LIBC}" = "newlib" ]; then
   NACLPORTS_CPPFLAGS+=" -I${NACLPORTS_INCLUDE}/glibc-compat"
-  export LIBS+=" -lglibc-compat"
+  LIBS+=" -lglibc-compat"
 fi
 
 # The transtive dependencies of libxpm include nacl_io which is
 # written in C++. Without this sxpm binary fails to link.
 if [ "${NACL_SHARED}" != "1" ]; then
-  export LIBS="-l${NACL_CPP_LIB} -pthread"
+  LIBS+=" -l${NACL_CPP_LIB} -pthread"
 fi
+
+export LIBS
