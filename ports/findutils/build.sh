ade1fbc7fe80f651b878ff6e133e9284f0c56a29
yipeng <tccyp@chromium.org>
2015-02-26 11:27:20 -0800

    Added port of findutils Please see the README file for the details of this port.
    R=sbc@chromium.org, bradnelson@chromium.org
    TEST=./make_all.sh findutils
    BUG=None
    
    Review URL: https://codereview.chromium.org/931283004

---
+++ ports/findutils/build.sh
@@ -0,0 +1,10 @@
+# Copyright 2015 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+BUILD_DIR=${SRC_DIR}
+export fu_cv_sys_mounted_fread=yes
+
+InstallStep() {
+  PublishByArchForDevEnv
+}
