1ac93e5dee8b07cd83d62c2df129009ebb00986a
Brad Nelson <bradnelson@google.com>
2015-04-15 12:53:27 -0700

    Drop linker flags from mingn linker script.
    Linker scripts group sections can only contain libraries.
    Listing cli_main and nacl_spawn explicitly rather than
    a variable that expands to include linker flags.
    
    Without this bare executables no longer link correctly in the devenv
    when passed -lmingn.
    
    BUG=None
    TEST=manual in devenv
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/1058253005

--- ports/mingn/build.sh
+++ ports/mingn/build.sh
@@ -122,12 +122,13 @@ toolchain/${OS_SUBDIR}_x86_glibc/x86_64-nacl/include
     # But also copy the debug ones.
     cp ${NACL_SDK_ROOT}/lib/glibc_${arch_alt}/Debug/* ${usr_lib_dir}/debug
 
-    local mingn_ldflags="${NACL_CLI_MAIN_LIB} -lppapi_simple -lnacl_io"
+    local mingn_ldflags="-lcli_main -lnacl_spawn -lppapi_simple -lnacl_io"
     mingn_ldflags+=" -lppapi -lstdc++ -lm"
     # Create libmingn.so ldscripts.
     cat <<EOF > ${TOOLCHAIN_OUT_DIR}/${arch}-nacl/usr/lib/libmingn.so
 OUTPUT_FORMAT(${ld_format})
 GROUP(${mingn_ldflags})
+EXTERN(PSUserMainGet)
 EOF
   done
 
