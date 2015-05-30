6fdf6673be09018f9d5de3a6c5fafeb2291d8215
Pete Williamson <petewil@chromium.org>
2015-05-29 18:08:41 -0700

    Rename the X-Windows version of emacs so I can tell them apart in the app launcher (and extension page).
    BUG=
    R=bradnelson@google.com
    
    Review URL: https://codereview.chromium.org/1164463006

--- ports/emacs-x/build.sh
+++ ports/emacs-x/build.sh
@@ -11,6 +11,7 @@ InstallStep() {
   local ASSEMBLY_DIR=${PUBLISH_DIR}/app
   MakeDir ${ASSEMBLY_DIR}
 
+  local EMACS_PORT_DIR=${START_DIR}/../emacs
   local EMACS_DIR=${NACL_PACKAGES_PUBLISH}/emacs/${TOOLCHAIN}/emacs
   local XORG_DIR=${NACL_PACKAGES_PUBLISH}/xorg-server/${TOOLCHAIN}/xorg-server
 
@@ -19,7 +20,6 @@ InstallStep() {
   LogExecute cp -r ${EMACS_DIR}/lib* ${ASSEMBLY_DIR}/
   LogExecute cp ${EMACS_DIR}/*.tar ${ASSEMBLY_DIR}/
   LogExecute cp ${EMACS_DIR}/*.png ${ASSEMBLY_DIR}/
-  LogExecute cp ${EMACS_DIR}/manifest.json ${ASSEMBLY_DIR}/
 
   LogExecute cp -r ${XORG_DIR}/_platform_specific ${ASSEMBLY_DIR}/
   LogExecute cp ${XORG_DIR}/*.nmf ${ASSEMBLY_DIR}/
@@ -30,4 +30,8 @@ InstallStep() {
   LogExecute cp ${XORG_DIR}/*.html ${ASSEMBLY_DIR}/
 
   LogExecute cp ${START_DIR}/Xsdl.js ${ASSEMBLY_DIR}/
+
+  LogExecute python ${TOOLS_DIR}/create_term.py -n EmacsXWindows emacs.nmf
+  GenerateManifest ${EMACS_PORT_DIR}/manifest.json ${ASSEMBLY_DIR} \
+    "TITLE"="EmacsXWindows"
 }
