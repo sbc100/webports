04bde82d81fd0d71241019a5a4e09993b8c15175
Petr Hosek <phosek@chromium.org>
2015-06-12 17:16:52 -0700

    libbsd port
    This library provides useful functions commonly found on BSD systems,
    and lacking on others like GNU systems, thus making it easier to port
    projects with strong BSD origins, without needing to embed the same code
    over and over again on each project.
    
    BUG= none
    TEST= ./make_all.sh libbsd
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/1178643011.

---
+++ ports/libbsd/build.sh
@@ -0,0 +1,10 @@
+# Copyright 2015 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+EXTRA_CONFIGURE_ARGS="--disable-largefile"
+
+if [ "${NACL_LIBC}" = "newlib" ]; then
+  NACLPORTS_CPPFLAGS+=" -I${NACLPORTS_INCLUDE}/glibc-compat"
+  export LIBS+=" -lglibc-compat"
+fi
