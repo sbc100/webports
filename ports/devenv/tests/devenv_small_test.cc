dda159afdf2194d134038aeb1d4297206c7fa159
bradnelson <bradnelson@google.com>
2015-07-31 16:59:42 -0700

    Adding a test of pipes chaining through multiple processes.
    BUG=None
    TEST=PipeChain
    R=gauravashish07@gmail.com,sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/1208423006

--- ports/devenv/tests/devenv_small_test.cc
+++ ports/devenv/tests/devenv_small_test.cc
@@ -318,6 +318,71 @@ TEST(Pipes, PipeFastClose) {
   EXPECT_EQ(0, close(pipe_b[0]));
 }
 
+// Write to a chain of echo process, get reply then close pipes.
+TEST(Pipes, EchoChain) {
+  int pipe_a[2];
+  ASSERT_EQ(0, pipe(pipe_a));
+  int pipe_b[2];
+  ASSERT_EQ(0, pipe(pipe_b));
+
+  pid_t pid = vfork();
+  ASSERT_GE(pid, 0);
+  if (!pid) {
+    // Dup two ends of the pipes into stdin + stdout of the echo process.
+    ASSERT_EQ(0, dup2(pipe_a[0], 0));
+    EXPECT_EQ(0, close(pipe_a[0]));
+    EXPECT_EQ(0, close(pipe_a[1]));
+    EXPECT_EQ(1, dup2(pipe_b[1], 1));
+    EXPECT_EQ(0, close(pipe_b[0]));
+    EXPECT_EQ(0, close(pipe_b[1]));
+    execlp(argv0, argv0, "pipes", NULL);
+    // Don't get here.
+    ASSERT_TRUE(false);
+  }
+  EXPECT_EQ(0, close(pipe_a[0]));
+  EXPECT_EQ(0, close(pipe_b[1]));
+
+  int pipe_c[2];
+  ASSERT_EQ(0, pipe(pipe_c));
+
+  pid_t pid2 = vfork();
+  ASSERT_GE(pid2, 0);
+  if (!pid2) {
+    // Dup two ends of the pipes into stdin + stdout of the echo process.
+    EXPECT_EQ(0, close(pipe_a[1]));
+    ASSERT_EQ(0, dup2(pipe_b[0], 0));
+    EXPECT_EQ(0, close(pipe_b[0]));
+    EXPECT_EQ(1, dup2(pipe_c[1], 1));
+    EXPECT_EQ(0, close(pipe_c[0]));
+    EXPECT_EQ(0, close(pipe_c[1]));
+    execlp(argv0, argv0, "pipes", NULL);
+    // Don't get here.
+    ASSERT_TRUE(false);
+  }
+  EXPECT_EQ(0, close(pipe_b[0]));
+  EXPECT_EQ(0, close(pipe_c[1]));
+
+  const char test_message[] = "test message";
+
+  // Write to pipe_a.
+  int len = write(pipe_a[1], test_message, strlen(test_message));
+  EXPECT_EQ(strlen(test_message), len);
+  char buffer[100];
+  // Wait for an echo back on pipe_c.
+  int total = 0;
+  while (total < strlen(test_message)) {
+    len = read(pipe_c[0], buffer + total, sizeof(buffer));
+    ASSERT_GE(len, 0);
+    if (len == 0) break;
+    total += len;
+  }
+  EXPECT_EQ(strlen(test_message), len);
+  EXPECT_TRUE(memcmp(buffer, test_message, len) == 0);
+
+  EXPECT_EQ(0, close(pipe_a[1]));
+  EXPECT_EQ(0, close(pipe_c[0]));
+}
+
 TEST(Pipes, NullFeof) {
   int p[2];
   ASSERT_EQ(0, pipe(p));
