7bef6ccd5c5878bd8d2ec76d02ecb4da4331bcff
sbc <sbc@chromium.org>
2015-09-25 15:13:42 -0700

    tar: Update upstream version
    TEST=manual testing in dev env
    
    Review URL: https://codereview.chromium.org/1360963002

--- ports/tar/build.sh
+++ ports/tar/build.sh
@@ -3,7 +3,7 @@
 # found in the LICENSE file.
 
 export EXTRA_LIBS="${NACL_CLI_MAIN_LIB}"
-EXECUTABLES=src/tar
+EXECUTABLES=src/tar${NACL_EXEEXT}
 
 # The default when cross compiling is to assume chown does not
 # follow symlinks, and the code that works around this in chown.c
@@ -18,6 +18,18 @@ if [ "${NACL_LIBC}" = "newlib" ]; then
   export LIBS="-lglibc-compat"
 fi
 
+if [ "${TOOLCHAIN}" = "glibc" -a "${NACL_ARCH}" != "arm" ]; then
+  # TODO(sbc): remove this once we fix glibc issue:
+  # https://code.google.com/p/nativeclient/issues/detail?id=4241
+  NACLPORTS_CPPFLAGS+=" -D_FORTIFY_SOURCE=0"
+fi
+
+if [ "${NACL_ARCH}" = "arm" ]; then
+  # The ARM compiler is a more recent version of gcc that generated a warnings
+  # (which are errors due to Werror) unless we disable them.
+  EXTRA_CONFIGURE_ARGS+=" --disable-gcc-warnings"
+fi
+
 if [ "${TOOLCHAIN}" = "pnacl" -o "${TOOLCHAIN}" = "clang-newlib" ]; then
   # correctly handle 'extern inline'
   NACLPORTS_CPPFLAGS+=" -std=gnu89"
@@ -25,7 +37,7 @@ fi
 
 PublishStep() {
   MakeDir ${PUBLISH_DIR}
-  cp src/tar ${PUBLISH_DIR}/tar_${NACL_ARCH}${NACL_EXEEXT}
+  cp src/tar${NACL_EXEEXT} ${PUBLISH_DIR}/tar_${NACL_ARCH}${NACL_EXEEXT}
   pushd ${PUBLISH_DIR}
   LogExecute python ${NACL_SDK_ROOT}/tools/create_nmf.py \
       ${PUBLISH_DIR}/tar_*${NACL_EXEEXT} \
