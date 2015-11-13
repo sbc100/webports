0b1b545e4bab3ab5f127fc8e8cec3cd7eaee1fb8
sbc <sbc@chromium.org>
2015-11-13 10:02:32 -0800

    Fix grep under glibc
    grep was previously relying on glibc's openat() family
    of functions which don't work (due to missing /proc).
    
    A second problem was with dup_safer which was also
    effect newlib and caused recursive grep to fail.
    
    Review URL: https://codereview.chromium.org/1439193002

--- ports/grep/build.sh
+++ ports/grep/build.sh
@@ -7,6 +7,18 @@ ConfigureStep() {
   NACLPORTS_LIBS+=" -l${NACL_CXX_LIB}"
   NACLPORTS_CPPFLAGS+=" -Dpipe=nacl_spawn_pipe"
 
+  if [ "${TOOLCHAIN}" = "glibc" ]; then
+    # Glibc's *at functions don't work correctly since they rely on
+    # /proc/self/fd/X.  This ensure thats gnulib's replacements are used
+    # instead.
+    export ac_cv_func_faccessat=no
+    export ac_cv_func_fstatat=no
+    export ac_cv_func_fchdir=no
+    export ac_cv_func_fdopendir=no
+
+    export gl_cv_func_open_directory_works=yes
+  fi
+
   # Grep fails to build NDEBUG defined
   # ib/chdir-long.c:62: error: unused variable 'close_fail'
   NACLPORTS_CFLAGS="${NACLPORTS_CFLAGS/-DNDEBUG/}"
